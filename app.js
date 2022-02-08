1bbfbbc2c337252f7c3c93729b9b3ca90fb7838c
patrickcordeiro <patrick.cordeiro30@gmail.com>
2022-02-07 22:50:48 -0300

    iniciando cookies

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require("express-session");
 
 var listaRouter = require('./routes/lista');
 var usersRouter = require('./routes/users');
@@ -13,6 +14,11 @@ var app = express();
 // view engine setu
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(session({
+  secret: "monstro", 
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(logger('dev'));
 app.use(express.json());
